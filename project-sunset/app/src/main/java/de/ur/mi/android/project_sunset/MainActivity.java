2dd3ce56a7ab71f377e5adfddc85853c0de24ca1
yannickhildebrandt <yannick@vuframelab.com>
2017-09-14 14:13:07 +0200

    added first versions of interfaces

--- project-sunset/app/src/main/java/de/ur/mi/android/project_sunset/MainActivity.java
+++ project-sunset/app/src/main/java/de/ur/mi/android/project_sunset/MainActivity.java
@@ -1,13 +1,37 @@
 package de.ur.mi.android.project_sunset;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    //Intent i = new Intent(this ,SecondScreen.class);
+    Button newCalculation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.entryscreen);
+        initUI();
+        setOnClickListener();
+
+
+    }
+
+    private void setOnClickListener() {
+        final Intent intent = new Intent(this,SecondScreen.class);
+        newCalculation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initUI() {
+        newCalculation = (Button) findViewById(R.id.button);
     }
 }
---
+++ project-sunset/app/src/main/java/de/ur/mi/android/project_sunset/SecondScreen.java
@@ -0,0 +1,23 @@
+package de.ur.mi.android.project_sunset;
+
+/**
+ * Created by yannickhildebrandt on 14/09/17.
+ */
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondScreen extends AppCompatActivity {
+    Button newCalculation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_screen);
+
+
+
+    }}
