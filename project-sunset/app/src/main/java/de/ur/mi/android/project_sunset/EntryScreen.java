eac8417c62339867513c596c773b3f62d8fda676
yannickhildebrandt <yannick@vuframelab.com>
2017-09-14 15:17:39 +0200

    added waypoints csv

--- project-sunset/app/src/main/java/de/ur/mi/android/project_sunset/EntryScreen.java
+++ project-sunset/app/src/main/java/de/ur/mi/android/project_sunset/EntryScreen.java
@@ -1,25 +1,69 @@
 package de.ur.mi.android.project_sunset;
 
 import android.content.Intent;
+import android.os.Build;
+import android.os.Debug;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class EntryScreen extends AppCompatActivity {
 
 
     Button newCalculation;
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.entryscreen);
         initUI();
+        Log.d("Hi","HI");
         setOnClickListener();
-
+        readCSV();
 
 
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void readCSV() {
+        String csvFile = "/src/main/res/files/waypoints.csv";
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+
+        try {
+
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+                String[] country = line.split(cvsSplitBy);
+                Log.d("Hi","TH");
+                System.out.println("Country [code= " + country[1] + " , name=" + country[2] + "]");
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+    }}
 
     private void setOnClickListener() {
         final Intent intent = new Intent(this,SecondScreen.class);
