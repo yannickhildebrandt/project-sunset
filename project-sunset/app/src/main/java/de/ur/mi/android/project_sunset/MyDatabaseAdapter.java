1b7c251a8981acbda6287b704e8cec408061273b
yannickhildebrandt <yannick@vuframelab.com>
2017-09-14 14:36:24 +0200

    added DatabaseAdapter

---
+++ project-sunset/app/src/main/java/de/ur/mi/android/project_sunset/MyDatabaseAdapter.java
@@ -0,0 +1,96 @@
+package de.ur.mi.android.project_sunset;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MyDatabaseAdapter {
+
+    private MyDatabaseHelper helper;
+    private SQLiteDatabase db;
+
+    // Datenbanksetup
+    public static final String DB_NAME = "my-app-db";
+    public static final int DB_VERSION = 1;
+
+    // Relationenmodell
+    // 3 Spalten in der Tabelle my-example-table
+    // _id, first-example, second-example
+    public static final String TABLE_EXAMPLE = "my-example-table";
+    public static final String KEY_ID = "_id";
+    public static final String KEY_FIRST_EXAMPLE = "first-example";
+    public static final String KEY_SECOND_EXAMPLE = "second-example";
+
+    public MyDatabaseAdapter(Context context) {
+        helper = new MyDatabaseHelper(context, DB_NAME, null, DB_VERSION);
+    }
+
+    // ÷ffnen der Datenbankverbindung
+    public void open() {
+        db = helper.getWritableDatabase();
+    }
+
+    // Schlieﬂen der Datenbankverbindung
+    public void close() {
+        db.close();
+        helper.close();
+    }
+
+    // Datenmanipulation: Methoden
+
+    // Beispielmethode: Objekt in my-example-table einf¸gen
+    public long insertMyObject(Object myExampleObject) {
+        // Datensammlung f¸r den einzuf¸genden Datensatz erstellen (ContentValues)
+        // nutzt Schl¸ssel-Wert-Mechanismus
+        // es werden die Konstanten v. o. genutzt, um Fehler zu vermeiden
+        ContentValues v = new ContentValues();
+        v.put(KEY_FIRST_EXAMPLE, myExampleObject.toString());
+        v.put(KEY_SECOND_EXAMPLE, myExampleObject.toString()); // exemparisch einfach toString()
+        long newInsertId = db.insert(TABLE_EXAMPLE, null, v);
+        return newInsertId;
+    }
+
+    // Beispielmethode: alle Eintr‰ge aus my-example-table holen
+    public Cursor getAllMyObjects() {
+        String[] allColumns = new String[] { KEY_ID, KEY_FIRST_EXAMPLE, KEY_SECOND_EXAMPLE };
+        Cursor results = db.query(TABLE_EXAMPLE, allColumns, null, null, null, null, null);
+        return results;
+    }
+
+
+    // Beispielmethode: Ein myObject-Tupel lˆschen
+    public void removeMyObject(long id) {
+        String toDelete = KEY_ID + "=?";
+        String[] deleteArgs = new String[] { String.valueOf(id) };
+        db.delete(TABLE_EXAMPLE, toDelete, deleteArgs);
+    }
+
+
+
+
+    // Interne Ableitung der Hilfsklasse SQLiteOpenHelper zur Erstellung der Tabellen
+    private class MyDatabaseHelper extends SQLiteOpenHelper {
+
+        // Hier wird ¸ber das SQL Statement das Datenmodell festgelegt
+        private static final String CREATE_DB = "create table " + TABLE_EXAMPLE + " (" + KEY_ID + " integer primary key autoincrement, " + KEY_FIRST_EXAMPLE + " text not null, " + KEY_SECOND_EXAMPLE + " text not null);";
+
+        public MyDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+            super(context, name, factory, version);
+        }
+
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(CREATE_DB);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            // Upgrage bei Versions‰nderung: Wie hat sich das Datenmodell ver‰ndert? Immer individuell je nach Datenbankversion!
+        }
+    }
+}
+
+
